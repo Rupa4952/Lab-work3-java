c579315921bb47dae6ff2fa6b31581144a1dc8f4
Rupa4952 <123296986+Rupa4952@users.noreply.github.com>
2023-02-24 08:43:26 -0800

    Update Person.java

--- lab 6/Person.java
+++ lab 6/Person.java
@@ -24,7 +24,7 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        Person student = new Person("Emon", 4971);
+        Person student = new Person("Rupa", 4952);
         System.out.println("Name :" + student.getName());
         System.out.println("ID :" + student.getId());
     }
